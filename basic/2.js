c378b7096ccf59bd1b11ac6f3ef26d01832aec95
archita1685 <156780101+archita1685@users.noreply.github.com>
2025-07-16 12:05:18 +0000

    hi

---
+++ basic/2.js
@@ -0,0 +1,11 @@
+let a=12;
+a=16;
+const b="archi";
+// b="saif";
+var c=23;
+c=20;
+d=89;
+// console.log(a);
+d=90;
+let e
+console.table([a,b,c,d,e]);
---
+++ basic/3.js
@@ -0,0 +1,5 @@
+"use strict" //tret all JS code as newer version
+//alert(3+3) // we re using node js not brower thats why this will giv us r` 
+console.log(typeof [1,2,3]);
+let a=Array.isArray([1,2,3]);
+console.log(a);
